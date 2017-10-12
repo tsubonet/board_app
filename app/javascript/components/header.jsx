4007b847b44f1c17bfb6dff8bdba31d053ed61f8
tsubonet <tsubonet@gmail.com>
2017-10-13 01:21:51 +0900

    g

--- app/javascript/components/header.jsx
+++ app/javascript/components/header.jsx
@@ -7,8 +7,8 @@ export default class Header extends React.Component {
       <header className="navbar navbar-default navbar-fixed-top">
         <div className="container">
           <div className="navbar-header">
-            <a href='/' className="navbar-brand"><i className="icon-male"></i> SexeS <i className="icon-female"></i></a>
-            <button className="navbar-toggle" data-toggle="collapse" data-target=".target">
+            <Link href='/' className="navbar-brand"><i className="icon-male"></i> SexeS <i className="icon-female"></i></Link>
+            <button className="navbar-toggle">
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
@@ -16,16 +16,15 @@ export default class Header extends React.Component {
           </div>
           <div className="collapse navbar-collapse target">
             <ul className="nav navbar-nav">
-              <li><a href='/topics/add'><i className="icon-comment"></i>&nbsp;質問する</a></li>
+              <li><a href='/topics/new'><i className="icon-comment"></i>&nbsp;質問する</a></li>
               <li><a href='/topics/?order=new'><i className="icon-user"></i>&nbsp;回答募集 <span className="badge"></span></a></li>
             </ul>
             <ul className="nav navbar-nav navbar-right">
               <li>
-              <form action="/topics" id="TopicIndexForm" method="post" accept-charset="utf-8">
-                <div><input type="hidden" name="_method" value="POST" /></div>
+              <form action="/topics" id="TopicIndexForm" method="post">
                 <div className="input-group">
-                  <label className="sr-only control-label" for="TopicTitle">Search</label>
-                  <input name="data[Topic][keyword]" className="form-control" placeholder="検索" maxlength="50" type="text" id="TopicTitle" required="required" />
+                  <label className="sr-only control-label" htmlFor="TopicSearch">Search</label>
+                  <input name="#" className="form-control" placeholder="検索" maxLength="50" type="text" id="TopicSearch" required />
                   <span className="input-group-btn"><button type="submit" className="btn btn-primary"><i className="icon-search"></i></button></span>
                 </div>
               </form>
