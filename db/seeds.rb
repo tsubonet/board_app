1c5ef5ad6574445e2ba47f5951a5749a487d7d8c
tsubonet <tsubonet@gmail.com>
2017-10-13 15:25:28 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do |i|
+  Topic.create(title: "タイトル #{i}", content: "本文 #{i}", gender: "man" )
+end
