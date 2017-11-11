a97d821e4020c38363d5d069f6594935d45761a1
tsubonet <tsubonet@gmail.com>
2017-11-11 22:41:05 +0900

    a

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :topic, touch: true
   belongs_to :user
   has_many :replies, dependent: :destroy
-  counter_culture :topic
   has_many :likes, -> { where(status: 'comment') }, foreign_key: :post_id, dependent: :destroy
-  
+  counter_culture :topic
+
   validates :content, presence: true, length: { maximum: 1000 }
 end
